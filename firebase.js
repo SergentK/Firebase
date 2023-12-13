0b41433ccf4a6a8d12bc1c0bfeacab9a717b545b
SergentK <116554131+SergentK@users.noreply.github.com>
2023-12-13 21:31:02 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from 'firebase/app'
 
 const app = initializeApp({
-  apiKey: "", // TODO
-  authDomain: "", // TODO
-  databaseURL: "", // TODO
-  projectId: "", // TODO
-  storageBucket: "" // TODO
+  apiKey: "AIzaSyBN41nUHWhK7JzWhtLkUuVLRhBudfsfTQY", // Dans les paramètres de firebase
+  authDomain: "revisions-cloud.firebaseapp.com", // Paramètre users/Domaine autorisés dans firebase
+ // databaseURL: "", // TODO
+  projectId: "revisions-cloud", // // Dans les paramètres de firebase
+ // storageBucket: "" // TODO
 })
 
 export default app
